93c4c5394372ce92a3fb73c2061bff405b28da2f
Dzianis <whitechapel2995@gmail.com>
2023-05-19 15:29:20 +0200

    cart api changes

--- Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -1,13 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
+    [ApiController]
     [Route("api/cart")]
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly ICartRepository _cartRepository;
+        private readonly ResponseDto _responseDto;
+
+        public CartController(ICartRepository cartRepository)
         {
-            return View();
+            _cartRepository = cartRepository;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> GetCart(string userId)
+        {
+            try
+            {
+                var cartDto = _cartRepository.GetCartByUserId(userId);
+                _responseDto.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _responseDto;
+        }
+
+        [HttpPost]
+        public async Task<object> AddCart(CartDto cartDto)
+        {
+            try
+            {
+                var cartDtoResult = _cartRepository.CreateUpdateCart(cartDto);
+                _responseDto.Result = cartDtoResult;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _responseDto;
+        }
+
+        [HttpPut]
+        public async Task<object> UpdateCart(CartDto cartDto)
+        {
+            try
+            {
+                var cartDtoResult = _cartRepository.CreateUpdateCart(cartDto);
+                _responseDto.Result = cartDtoResult;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _responseDto;
+        }
+
+        [HttpDelete]
+        public async Task<object> RemoveCart([FromBody]int cartId)
+        {
+            try
+            {
+                var isSuccess = _cartRepository.RemoveFromCart(cartId);
+                _responseDto.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _responseDto;
         }
     }
 }
--- Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public int CardHeaderId { get; set; }
-        public virtual CartHeader CartHeader { get; set; }
+        public virtual CartHeaderDto CartHeader { get; set; }
         public int ProductId { get; set; }
-        public virtual Product Product { get; set; }
+        public virtual ProductDto Product { get; set; }
         public int Count { get; set; }
     }
 }
