93ab043f80cb3f6526c7df0c4fbb27e7b1a96c0f
Dzianis <whitechapel2995@gmail.com>
2023-06-03 20:42:12 +0200

    update connection string to app configuration

--- Mango.Service.ProductAPI/Program.cs
+++ Mango.Service.ProductAPI/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 var dbConnectionString = string.Empty;
 
 builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
-string appSettingConnectionString = builder.Configuration["AppConfigConnectionString2"];
+string appSettingConnectionString = builder.Configuration["AppConfigConnectionString"];
 builder.Configuration.AddAzureAppConfiguration(options =>
 {
     options.Connect(appSettingConnectionString)
