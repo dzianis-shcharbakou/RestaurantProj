d84d80bbdecf20dba5f53dfab23b77c48cd893ed
Dzianis <whitechapel2995@gmail.com>
2023-06-20 17:48:55 +0200

    add migration

--- Mango.Service.ProductAPI/Migrations/20230620154803_AddPriceDbType.Designer.cs
+++ Mango.Service.ProductAPI/Migrations/20230620154803_AddPriceDbType.Designer.cs
@@ -3,6 +3,7 @@ using Mango.Service.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Service.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230620154803_AddPriceDbType")]
+    partial class AddPriceDbType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mango.Service.ProductAPI/Migrations/20230620154803_AddPriceDbType.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Service.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceDbType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://product73oktiews6luy.blob.core.windows.net/mango-product/14.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://product73oktiews6luy.blob.core.windows.net/mango-product/12.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://product73oktiews6luy.blob.core.windows.net/mango-product/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://product73oktiews6luy.blob.core.windows.net/mango-product/13.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://whitechapel5991.blob.core.windows.net/mango-product/14.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://whitechapel5991.blob.core.windows.net/mango-product/12.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://whitechapel5991.blob.core.windows.net/mango-product/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://whitechapel5991.blob.core.windows.net/mango-product/13.jpg");
+        }
+    }
+}
